dd6a128889a8d539292e653a79347b056537878f
athul-temp-gcp <athul.temp.gcp@gmail.com>
2024-09-24 18:05:27 +0200

    Update backends.tf

--- backends.tf
+++ backends.tf
@@ -1,13 +1,1 @@
-terraform {
-  backend "gcs" {
-    bucket = "terraform-state-999444333"
-    prefix = "terraform/state/dev"
-  }
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.60"
-    }
-  }
-}
