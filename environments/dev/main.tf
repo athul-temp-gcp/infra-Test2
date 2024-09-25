394196443cdacebaa7c9148ba8812e8636882cd6
athul-temp-gcp <athul.temp.gcp@gmail.com>
2024-09-25 16:28:35 +0200

    Create main.tf

---
+++ environments/dev/main.tf
@@ -0,0 +1,37 @@
+resource "google_compute_instance" "vmtest01" {
+  name         = "vmtest01-instance"
+  project      = var.gcp.project
+  zone         = "${var.gcp.region}-a"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral external IP
+    }
+  }
+
+}
+
+resource "google_compute_firewall" "vmtest01_firewall" {
+  name    = "vmtest01-firewall"
+  project = var.gcp.project
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
