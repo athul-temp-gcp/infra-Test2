d113bb52f4ca7caee2f2bd1f58a684ce0fc76261
athul-temp-gcp <athul.temp.gcp@gmail.com>
2024-10-03 12:34:27 +0200

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "vmtest01" {
   name         = "vmtest01-instance"
   project      = var.gcp.project
   zone         = "${var.gcp.region}-b"
-  machine_type = "n1-standard-1"
+  machine_type = "e2-medium"
 
   boot_disk {
     initialize_params {
